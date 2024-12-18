abdbf0281781c2e165f06d44dbbb9fc105934145
Arpit Jana <arpitjana2103@gmail.com>
2024-12-18 19:06:14 +0530

    styling heading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+const headingStyle = {
+    textAlign: "center",
+    textTransform: "uppercase",
+    fontWeight: "400",
+    paddingBottom: "3rem",
+    color: "#edc84b",
+};
+
 function Header() {
     return (
         <div>
-            <h1>Fast React Pizza Co.</h1>
+            <h1 style={headingStyle}>Fast React Pizza Co.</h1>
         </div>
     );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,10 +5,19 @@ const menuBoxStyle = {
     gridTemplateColumns: "repeat(2, 1fr)",
 };
 
+const menuHeaderStyle = {
+    textTransform: "uppercase",
+    padding: "1rem 0",
+    borderTop: "3px solid #27272a",
+    borderBottom: "3px solid #27272a",
+    display: "inline-block",
+    margin: "0 auto",
+};
+
 function Menu() {
     return (
-        <div>
-            <h2>Our Menu</h2>
+        <>
+            <h2 style={menuHeaderStyle}>Our Menu</h2>
             <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Deleniti, possimus laudantium! Itaque unde ea impedit nemo porro
@@ -23,7 +32,7 @@ function Menu() {
                 <Pizza />
                 <Pizza />
             </div>
-        </div>
+        </>
     );
 }
 
